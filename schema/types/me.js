df6acfcd991428e041a8a5bf89629a0f2c35298a
Lydia Hendriks <lhendriks@exzeo.com>
2020-10-02 18:12:34 -0400

    Querying postgres and mongo!! :tada:

--- schema/types/me.js
+++ schema/types/me.js
@@ -3,11 +3,13 @@ const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
+    GraphQLInt,
     GraphQLNonNull,
     GraphQLList
 } = require('graphql');
 
 const pgdb = require('../../database/pgdb')
+const mdb = require('../../database/mdb')
 const ContestType = require('./contest')
 
 module.exports = new GraphQLObjectType({
@@ -15,7 +17,7 @@ module.exports = new GraphQLObjectType({
     fields: {
         id: { type: GraphQLID }, // GraphQLID is a special scalar type to represent a unique value
         email:{ type: new GraphQLNonNull(GraphQLString)}, // per db design email is required
-        // firstName: { // thjs would work but will become repetitive
+        // firstName: { // thjs would work but will become repetitive so put logic in pgdb.js instead
         //     type: GraphQLString,
         //     resolve: obj => obj.first_name
         // },
@@ -26,6 +28,24 @@ module.exports = new GraphQLObjectType({
             resolve: obj => `${obj.firstName} ${obj.lastName}`
         },
         createdAt: {type: GraphQLString},
+        contestsCount: {
+            type: GraphQLInt,
+            resolve(obj, args, {mPool}, {fieldName}) {
+                return mdb(mPool).getCounts(obj, fieldName)
+            }
+        },
+        namesCount: {
+            type: GraphQLInt,
+            resolve(obj, args, {mPool}, {fieldName}) {
+                return mdb(mPool).getCounts(obj, fieldName)
+            }
+        },
+        votesCount: {
+            type: GraphQLInt,
+            resolve(obj, args, {mPool}, {fieldName}) {
+                return mdb(mPool).getCounts(obj, fieldName)
+            }
+        },
         contests: {
             type: new GraphQLList(ContestType), // GraphQLList is a special type modifier for arrays
             resolve(obj, args, {pool} ) {
